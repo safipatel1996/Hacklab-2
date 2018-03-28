65785fae89d2c5765ba29f65a5d99acd478e9f67
safipatel1996 <patelm33@uni.coventry.ac.uk>
2018-03-28 13:04:03 +0100

    Arduino Code_Lamp
    Weather Code for arduino neo pixel 24rgb

---
+++ strandtest_lamp.2/strandtest_lamp.2.ino
@@ -0,0 +1,161 @@
+
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PIN 6
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, PIN, NEO_GRB + NEO_KHZ800);
+float rr, gg, bb;
+
+char city;
+const char HEADER   = 'H';
+const char A_TAG    = 'M';
+const char B_TAG    = 'X';
+const int  TOTAL_BYTES  = 10  ; // the total bytes in a message
+
+int a, b, c, d;
+
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+  rr = 0;
+  gg = 0;
+  bb = 0;
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  colorWipe(strip.Color(125, 125, 125), 50);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if ( Serial.available()>=TOTAL_BYTES){
+    if (Serial.read() == HEADER){
+      char tag = Serial.read();
+      if (tag == A_TAG){
+        //Collect integers
+        a = Serial.read() * 256;
+        a = a + Serial.read();
+        b = Serial.read() * 256;
+        b = b + Serial.read();
+        c = Serial.read() * 256;
+        c = c + Serial.read();
+        d = Serial.read() * 256;
+        d = d + Serial.read();
+
+        Serial.print("Received integers | a:");
+        Serial.print(a);
+        Serial.print(", b:");
+        Serial.println(b);
+        Serial.print(", c:");
+        Serial.println(c);
+        Serial.print(", d:");
+        Serial.println(d);
+
+        colorWipe(strip.Color(a, b, c), 50);
+      }
+      else {
+        // Serial.print("got message with unknown tag ");
+        //Serial.write(tag);
+      }
+    }
+
+    
+
+  }
+
+}
+
+
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+    for (int q = 0; q < 3; q++) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, c);  //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
+    for (int q = 0; q < 3; q++) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, Wheel( (i + j) % 255)); //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
